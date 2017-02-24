1e94a53268ed3debd86a5900b761ddaa1b0391b9
deanfoulds <deanfoulds@gmail.com>
2017-02-24 13:15:08 +0000

    Solution to problem 2

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,13 +1,15 @@
 
-function fibonacci(num){
-  var a = 1, b = 0, temp;
+var previous = 0;
+var current = 1;
+var sum = 0;
+var next;
 
-  while (num >= 0){
-    temp = a;
-    a = a + b;
-    b = temp;
-    num--;
-  }
-
-  return b;
-}
+   for(i = 1; i < 100; i++){
+        next = current + previous;
+        previous = current;
+        current = next; 
+        if(current % 2 === 0 && current < 4000000) {
+            sum += current;
+        console.log(sum);
+        }
+   }
